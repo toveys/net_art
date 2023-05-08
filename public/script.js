16ea6429a1a73aa3e0bcb351e17bfedfb79745c7
Sean Tovey <S3919716@student.rmit.edu.au>
2023-05-08 11:29:41 +1000

    fractal patter

--- public/script.js
+++ public/script.js
@@ -1,36 +1,50 @@
 // get rid of the default document padding
-document.body.style.margin   = 0
-document.body.style.overflow = `hidden`
+document.body.style.margin   = 0;
+document.body.style.overflow = "hidden";
+//Create canvas and make it the length and width of the browser window
+const cnv = document.getElementById ("example_canvas");
+cnv.style.background = "turquoise";
+const ctx = cnv.getContext ("2d");
+resize_canvas ();
+window.onresize = resize_canvas;
 
-const cnv = document.getElementById (`example_canvas`)
-cnv.style.background = "turquoise"
+//Declare variables
+var angle = Math.PI/4;
+var x_pos = 0; 
+//Call Draw function
+requestAnimationFrame(draw);
+function draw(){
+    cnv.style.background = "turquoise";
+    ctx.lineWidth = 10;
+    ctx.strokeStyle = "white";
+    //translate canvas origin point
+    ctx.translate(cnv.width/2, cnv.height);
+    branch(200);
+    ctx.setTransform(1, 0, 0, 1, 0, 0);
+}
 
-const ctx = cnv.getContext (`2d`)
-
-resize_canvas ()
-
-window.onresize = resize_canvas
-
-let x_pos = 0
-
-requestAnimationFrame (draw_frame)
-
-function draw_frame () {
-    ctx.fillStyle = `turquoise`
-    ctx.fillRect(0, 0, cnv.width, cnv.height)
-
-    ctx.fillStyle = `deeppink`
-    ctx.fillRect(x_pos % 1 * cnv.width, 150, 100, 100)
-
-    x_pos += .001;
-
-    if(x_pos > cnv.width){
-        x_pos = 0;
+function branch(len){
+    //draw line starting from canvas origin to -len in the y direction
+    ctx.moveTo(0, 0);
+    ctx.lineTo(0, -len);
+    ctx.stroke();
+    //translate to tip of the line
+    ctx.translate(0, -len);
+    if (len > 4){
+        ctx.save();
+        //rotate
+        ctx.rotate(angle);
+        branch(len*.67);
+        ctx.restore();
+        ctx.save();
+        //rotate
+        ctx.rotate(-angle);
+        branch(len*.67);
+        ctx.restore();
     }
-    requestAnimationFrame (draw_frame)
 }
 
 function resize_canvas() {
-    cnv.width = innerWidth
-    cnv.height = innerHeight
+    cnv.width = innerWidth;
+    cnv.height = innerHeight;
 }
